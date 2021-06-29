1affff25baed2d6d3be0cb2ff0ec7bbccd55efe3
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2021-06-29 15:50:01 +0530

    Added remote SQL connection

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'portfolio');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'portfolio');
+
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'bwdVjv2xHE');
+define('DB_PASSWORD', 'hofhBDePT7');
+define('DB_NAME', 'bwdVjv2xHE');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
